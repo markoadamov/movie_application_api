9cc1e96e102c3500a51d024f63f8f81336a8c563
MarkoAdamov <markoadamov.zr@gmail.com>
2023-06-01 11:39:41 +0200

    Authentication

--- movie_project/urls.py
+++ movie_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
